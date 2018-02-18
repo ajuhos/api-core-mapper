50798efc17f10121b7a815ea019d02f0c79727a1
Juhos Ádám <ajuhos@logiscool.com>
2018-02-18 14:03:06 +0100

    Remove ignored files

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { ApiMapper } from "./src/ApiMapper";
-export { ApiSwaggerMapper } from "./src/ApiSwaggerMapper";
-export { ApiMapProvider } from "./src/ApiMapProvider";
-export { ApiSwaggerProvider } from "./src/ApiSwaggerProvider";
